7b13dc6be3a3465f35f07f714f90483ec51dbe69
Mateus Borges Rodrigues <mateusborges090@gmail.com>
2024-11-13 10:35:22 -0300

    Initial Commit

---
+++ FinanceTips/FinanceTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTipsApp.swift
+//  FinanceTips
+//
+//  Created by Mateus on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceTipsTests/FinanceTipsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTipsTests.swift
+//  FinanceTipsTests
+//
+//  Created by Mateus on 13/11/24.
+//
+
+import Testing
+@testable import FinanceTips
+
+struct FinanceTipsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
